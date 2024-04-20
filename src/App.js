0e3c529da343d5324fd89fe75f6073082e86e710
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2024-04-21 01:39:59 +0200

    Component CategoryList & Axios for APIs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CategoryList from "./CategoryList/categoryList";
 import Header from "./Header/header";
 
 function App() {
   return (
     <div className="wrapper bg-faded-dark">
       <Header></Header>
+      <CategoryList></CategoryList>
     </div>
   );
 }
