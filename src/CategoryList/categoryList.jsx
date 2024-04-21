48ba67d7d55b022057ee1ddfc5ed4458eafddaf5
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2024-04-21 03:59:24 +0200

    Category List

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,15 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../axios";
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
-      console.log(response.data);
+      // console.log(response.data);
+      setCategories(response.data);
     };
     fetchCategories();
   }, []);
-  return <div>Category List</div>;
+  return (
+    <nav className=" container mt-n5 d-flex align-items-center">
+      <div
+        className="bg-white rounded-3 shadow-lg py-4"
+        style={{ height: "80px" }}
+      >
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              همه فست فود ها
+            </a>
+          </li>
+          {categories.map((category) => (
+            <li className="nav-item" key={category.id}>
+              <a className="nav-link" href="#">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default CategoryList;
--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+// const cors = require("cors");
+// App.use(
+//   cors({
+//     origin: "http://localhost:3001", //
+//   })
+// );
 
 export default axios.create({
   baseURL: "https://react-mini-projects-api.classbon.com/",
